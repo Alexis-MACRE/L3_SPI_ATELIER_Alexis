5a7d20c167e9d04d42a9bdeda10285d85833e8ef
Ghinevra <71076162+Ghinc@users.noreply.github.com>
2020-09-13 07:07:04 +0200

    Update Ex3.py

--- Atelier 1/Ex3.py
+++ Atelier 1/Ex3.py
@@ -8,11 +8,13 @@ def imposable() :
     """Cette fopnction permet de verifier si un citoyen est imposable en fontion de l'age et du sexe"""
     sexe=input("Saisir homme ou femme (H ou F):" )
     age=int(input("Saisir age :"))
-    if sexe=="H" and age>20:
+    if sexe=="H" and age>20: #vous auriez pu faire plus court (en mettant les deux dans une seule proposition par ex)
         imposable=True
-    elif sexe=="F" and age>=18 and age <=35:
+    elif sexe=="F" and age>=18 and age <=35: #pareil que dans l'exo du salaire, vous auriez pu mettre des constantes (pour les âges notamment)
         imposable=True
     else :
         imposable=False
     print(imposable)
 imposable()
+
+#plutôt que if.... imposable = true, vous auriez directeemnt pu mettre imposable = (la condition)
